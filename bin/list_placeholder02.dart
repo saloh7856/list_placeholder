dabf993f73c3eaf845fdbd79c07abe24990899c1
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-14 16:02:43 +0500

    list

---
+++ bin/list_placeholder02.dart
@@ -0,0 +1,17 @@
+/*
+Create function func 
+Make a list of n  zeros.
+Argument int n
+Return the List
+
+*/
+
+List func(int n){
+    List y=List.filled(n,0);
+    return y; 
+}
+
+void main(){   
+print(func(3));
+
+}
