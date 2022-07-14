43d9912b93842018f2e8e49e9b6625df32623245
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-14 15:47:09 +0500

    list yaratib chop etish

--- bin/list_placeholder01.dart
+++ bin/list_placeholder01.dart
@@ -4,5 +4,12 @@ Create a list with the following values: 1, 2, 3, 4, 5
 Return the List
 
 */
+List func(List a){
+    
+    return a; 
+}
 
-void main() {}
+void main(){   
+print(func([1,2,3,4,5]));
+
+}
