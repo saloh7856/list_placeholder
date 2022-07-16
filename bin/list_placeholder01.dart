2705e9019f27a5361cc817af10edfbc8963640e5
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-16 09:26:17 +0500

    01

--- bin/list_placeholder01.dart
+++ bin/list_placeholder01.dart
@@ -4,12 +4,12 @@ Create a list with the following values: 1, 2, 3, 4, 5
 Return the List
 
 */
-List func(List a){
+List func(){
     
-    return a; 
+    return [1,2,3,4,5]; 
 }
 
 void main(){   
-print(func([1,2,3,4,5]));
+print();
 
 }
