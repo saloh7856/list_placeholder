f0793c22dd96b99d553bd1590f6086e534fc7a38
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-16 13:24:12 +0500

    10

--- bin/list_placeholder10.dart
+++ bin/list_placeholder10.dart
@@ -6,4 +6,21 @@ Return the List
 
 */
 
-void main() {}
+List func(int n){
+    List x = [];
+    int k=0;
+    while (k < n){
+        
+        if(k%2==0){
+        k.toString();
+        }
+    x.add(k);
+        k+=1;
+    }
+    return x; 
+}
+
+void main(){   
+print(func(6));
+
+}
